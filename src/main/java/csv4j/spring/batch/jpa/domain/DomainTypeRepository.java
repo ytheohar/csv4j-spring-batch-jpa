9dbf70c86a7a3fcb307a3b8277b9dd774689643e
Yannis Theocharis <ytheohar@gmail.com>
2015-06-13 14:46:20 +0100

    I remove the CRUD repository as it is not needed for this example. I also add new entries into the gitignore

--- src/main/java/csv4j/spring/batch/jpa/domain/DomainTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package csv4j.spring.batch.jpa.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DomainTypeRepository extends
-		CrudRepository<DomainType, Long> {
-
-}
